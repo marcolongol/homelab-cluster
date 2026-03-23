5257d45b238e3ed58902612e5e97d6cd712b92fc
Lucas Marcolongo <lucas@marcolongo.dev>
2026-03-22 21:07:35 -0700

    style(nix): reformat flake.nix and add mise to dev shell

--- flake.nix
+++ flake.nix
@@ -5,28 +5,27 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          # Python build dependencies
-          gcc
-          gnumake
-          pkg-config
-          zlib
-          openssl
-          readline
-          bzip2
-          libffi
-          ncurses
-          xz
-          sqlite
-          pipx
-        ];
-      };
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        # Python build dependencies
+        gcc
+        gnumake
+        pkg-config
+        zlib
+        openssl
+        readline
+        bzip2
+        libffi
+        ncurses
+        xz
+        sqlite
+        pipx
+        mise
+      ];
     };
+  };
 }
